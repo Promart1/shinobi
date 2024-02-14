4ca8b132ccf69a2327d8126214b3e955701abea3
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-02-14 23:03:23 +0200

    add part of analysis section

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const menuClose = document.querySelector('.menu__close');
 const menuList = document.querySelector('.header__list');
 const menuShadow = document.querySelector('.menu--close')
 
+
 menuBtn.addEventListener('click', ()=>{
     menuList.classList.toggle('menu__list--open');
     menuShadow.classList.toggle('menu--open');
@@ -13,6 +14,33 @@ menuClose.addEventListener('click', ()=>{
     menuShadow.classList.remove('menu--open');
 });
 
-// function toggleActive(element) {
-//     element.classList.toggle('active');
-// }
+
+ const swiper = new Swiper('.analysis-slider', {
+        loop: true,                         
+        slidesPerView: 2,                   
+     centeredSlides: true,   
+    effect: "coverflow",
+    coverflowEffect: {          
+        rotate: 0,              
+        depth: 200,             
+        stretch: 5,            
+        modifier: 1,            
+        scale: 1,               
+        slideShadows: false,    
+    },     
+     spaceBetween: 21, 
+     breakpoints: {
+          1024: {
+             spaceBetween: 40
+         },
+         426: {
+             spaceBetween: 80,
+            //  coverflowEffect: {
+            //      depth: 200,
+            //  } 
+         },
+     },
+        autoplay: {                         
+            delay: 2000,  
+        },   
+    })
